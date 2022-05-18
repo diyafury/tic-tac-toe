e96b46b27a36e29a1618e416fdb7ce3f9b392bf6
Diyafury <diyafury@googlemail.com>
2022-05-18 22:35:48 +0200

    added base css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import 'https://diyafury.github.io/works/css/base.css';
+import './base.css';
 
 function Square(props) {
     return (
